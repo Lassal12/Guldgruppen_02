ae1965e778a735c23a58970098163434cb91c251
SigurdEEspersen <32567367+SigurdEEspersen@users.noreply.github.com>
2017-10-25 12:27:07 +0200

    første commit for npc

--- src/NPC.java
+++ src/NPC.java
@@ -1,4 +1,44 @@
+import java.util.HashMap;
+import java.util.Set;
+
 
 public class NPC {
+    private String name;
+    private String greeting;
+    private HashMap<String, Swag> inventory;
     
+    
+    public NPC(String name, String greeting) {
+        this.name = name;
+        this.greeting = greeting;
+        inventory = new HashMap<String, Swag>();
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getGreeting() {
+        return name + ": " + greeting + "\n";
+    }
+    
+    public Swag getSwag(String swag) {
+        return inventory.get(swag);
+    }
+    
+    public void addSwagToInventory(Swag swag) {
+        inventory.put(swag.getSwagDescription(), swag);
+    }
+    
+    public void removeSwagFromInventory(String swag) {
+        inventory.remove(swag);
+    }
+    
+    public boolean isInInventory(String swagName) {
+        return inventory.containsKey(swagName);
+    }
+    
+    public void setGreeting(String response) {
+        this.greeting = response;
+    }
 }
